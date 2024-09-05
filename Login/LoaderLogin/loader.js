280654dbcbce40c6f407325db7bb101a9fdee29b
Gaston <leitnergaston@gmail.com>
2024-09-04 22:48:08 -0300

    creacion de loader
    se creo un loader para el inicio, el login, el word y excel y camaras. se quitaron los node module que instale de npm boostrap

---
+++ Login/LoaderLogin/loader.js
@@ -0,0 +1,17 @@
+// Loader sin tiempo de espera
+// espera el tiempo carga de la pagina para que desaparezca el loader
+// window.addEventListener('load', function() {
+    // Ocultar el loader una vez que la página se cargue completamente
+    // const loader = document.getElementById('loader');
+    // loader.style.display = 'none';
+// });
+
+// loader con tiempo de espera
+// espera el tiempo que carga la pagina y le agrega 1 segundo mas
+// antes de desaparecer el loader
+window.addEventListener('load', function() {
+    setTimeout(function() {
+        const loader = document.getElementById('loader');
+        loader.style.display = 'none';
+    }, 200); // Espera 1 segundos antes de ocultar el loader
+});
