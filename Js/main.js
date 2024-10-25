f7f531969df8b45690476c55ca34218a75d2f115
Gaston <leitnergaston@gmail.com>
2024-10-24 21:46:12 -0300

    probando scroll reveal con netlify
    para ver si el deploy en netlify funciona scroll reveal publicado en la web

--- Js/main.js
+++ Js/main.js
@@ -2,3 +2,17 @@
 function goBack() {
     window.history.back();
 }
+
+
+// probando scroll reveal con deploy en netlify
+// Scroll Reveal
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 5000,
+    reset: false
+});
+
+sr.reveal('.container-content',{delay:300});
+sr.reveal('.card',{});
+sr.reveal('.contact',{delay:500});
