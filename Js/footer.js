8a8e21b92f45b76a608f70c803042e48caa58a42
Gaston <leitnergaston@gmail.com>
2024-10-26 18:55:50 -0300

    cambios en footer y ver contraseña en logins

--- Js/footer.js
+++ Js/footer.js
@@ -1,10 +1,16 @@
 // Definir la fecha de la última actualización
-var ultimaActualizacion = "Última Actualización 3-Oct-24";
+var ultimaActualizacion = "Última Actualización 26-Oct-24";
+var copy = "GC CodeDevs";
 
 // Actualizar el contenido del footer en todas las páginas
 document.addEventListener("DOMContentLoaded", function() {
     var footerText = document.getElementById("footer-date");
     if (footerText) {
         footerText.textContent = ultimaActualizacion;
+    }
+
+    var copyText = document.getElementById("copy-text");
+    if (copyText) {
+        copyText.textContent = copy;
     }
 });
